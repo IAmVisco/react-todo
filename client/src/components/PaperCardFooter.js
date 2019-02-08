0cce54898ccae902931f98103d936729684f6ab7
Alexander <sashok935@gmail.com>
2019-02-08 20:10:25 +0300

    Add WIP dropdown

--- client/src/components/PaperCardFooter.js
+++ client/src/components/PaperCardFooter.js
@@ -2,15 +2,27 @@ import React from 'react'
 import Moment from 'react-moment'
 import FontAwesome from 'react-fontawesome'
 import {icon, status} from '../data/consts'
+import {Dropdown} from "react-bootstrap"
 
 class PaperCardFooter extends React.Component {
   render() {
     return (
       <div className="paper-card-footer">
-        <span className="small">
+        <span className="small" style={{'cursor': 'pointer'}}>
           <FontAwesome name={icon[this.props.status]}/>
           {status[this.props.status]}
         </span>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <span className="text-muted small pull-right">Created at <Moment
           format="MMM Do YYYY">{this.props.createdAt}</Moment></span>
       </div>
